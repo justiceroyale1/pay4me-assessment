ab89b6fb9dda02e85c936e8bfb660cdc428e6242
Abutu Justice Royale <justiceroyale1@gmail.com>
2025-03-10 15:08:02 +0100

    Add seeders

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PopulateUsersTable extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
